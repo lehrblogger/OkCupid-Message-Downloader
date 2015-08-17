d16be31441fe2393bb510d67f92def9f5e5d9fda
Steven Lehrburger <lehrburger@gmail.com>
2015-08-16 23:42:29 -0400

    Add necessary files for publishing to PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'OkCupid-Message-Downloader',
+  packages = ['OkCupid-Message-Downloader'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A simple Python script for downloading your sent and received OkCupid messages.',
+  author = 'Steven Lehrburger',
+  author_email = 'lehrburger@gmail.com',
+  url = 'https://github.com/lehrblogger/OkCupid-Message-Downloader',
+  download_url = 'https://github.com/lehrblogger/OkCupid-Message-Downloader/tarball/0.1',
+  keywords = ['okcupid', 'okc', 'dating', 'archive', 'download', 'backup'],
+  classifiers = [],
+)
+
