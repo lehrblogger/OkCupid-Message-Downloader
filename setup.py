86f66d5b7faef6396828d1dc473dec58125db421
Steven Lehrburger <lehrburger@gmail.com>
2015-08-25 00:11:22 -0400

    Use setuptools instead of distutils #28
    I was getting `UserWarning: Unknown distribution option` warnings for `entry_points` and `install_requires`.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'okcmd',
   packages = ['okcmd'], # this must be the same as the name above
   entry_points = {'console_scripts': [
       'okcmd = okcmd.arrow_fetcher:main',
   ],},
-  version = '0.10',
+  version = '0.11',
   description = 'A simple Python script for downloading your sent and received OkCupid messages.',
   author = 'Steven Lehrburger',
   author_email = 'lehrburger@gmail.com',
   url = 'https://github.com/lehrblogger/OkCupid-Message-Downloader',
-  download_url = 'https://github.com/lehrblogger/OkCupid-Message-Downloader/tarball/0.10',
+  download_url = 'https://github.com/lehrblogger/OkCupid-Message-Downloader/tarball/0.11',
   keywords = ['okcupid', 'okc', 'okcmd', 'dating', 'archive', 'message', 'download', 'backup'],
   install_requires = ['beautifulsoup4==4.4.0'],
   classifiers = [
