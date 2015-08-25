802af4e9041f01e8854c34f36a5e213d44139310
Steven Lehrburger <lehrburger@gmail.com>
2015-08-24 23:53:39 -0400

    Argh I forgot to add install_requires bs4 #28

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ setup(
   entry_points = {'console_scripts': [
       'okcmd = okcmd.arrow_fetcher:main_func',
   ],},
-  version = '0.7',
+  version = '0.8',
   description = 'A simple Python script for downloading your sent and received OkCupid messages.',
   author = 'Steven Lehrburger',
   author_email = 'lehrburger@gmail.com',
   url = 'https://github.com/lehrblogger/OkCupid-Message-Downloader',
   download_url = 'https://github.com/lehrblogger/OkCupid-Message-Downloader/tarball/0.1',
   keywords = ['okcupid', 'okc', 'okcmd', 'dating', 'archive', 'message', 'download', 'backup'],
+  install_requires = ['beautifulsoup4==4.4.0'],
   classifiers = [
       'Development Status :: 3 - Alpha',
       'License :: OSI Approved :: MIT License',
