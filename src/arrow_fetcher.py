4f6833dadba4d0b4adf6fc8db0ffbe91818be05e
Steven Lehrburger <lehrburger@gmail.com>
2015-09-14 01:16:20 -0400

    Bump version number to 1.2 for various improvements #31 #32

--- src/arrow_fetcher.py
+++ src/arrow_fetcher.py
@@ -297,7 +297,7 @@ def main():
     description = "OkCupid-Message-Downloader (OKCMD): a tool for downloading your sent and received OkCupid messages to a text file."
     epilog = "See also https://github.com/lehrblogger/OkCupid-Message-Downloader"
     # TODO: add version argument based on setup.py's version number.
-    #version = "okcmd 1.1"
+    #version = "okcmd 1.2"
     parser = OptionParser(usage=usage, description=description, epilog=epilog)
     parser.add_option("-u", "--username", dest="username",
                       help="your OkCupid username")
